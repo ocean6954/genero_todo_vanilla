b4c6a50e86439ce86d3e69dced32b85fc1a5dced
ocean6954 <oceanan699@gmail.com>
2025-10-02 16:08:42 +0900

    削除機能と作成機能の追加

--- todo.js
+++ todo.js
@@ -1,14 +1,27 @@
-const input = document.getElementById('todo-input');
-const button = document.getElementById('add-button');
-const list = document.getElementById('todo-list');
+const input = document.getElementById("todo-input");
+const button = document.getElementById("add-button");
+const list = document.getElementById("todo-list");
 
-button.addEventListener('click', () => {
+let list_id = 0;
+
+button.addEventListener("click", () => {
   const text = input.value;
   if (!text) return;
 
-  const li = document.createElement('li');
+  const li = document.createElement("li");
+  const del = document.createElement("button");
+  console.log(del);
   li.textContent = text;
+  li.id = list_id;
+  del.textContent = "削除する";
+  del.addEventListener("click", () => {
+    const target = document.getElementById(li.id);
+    target.remove();
+  });
+
+  li.appendChild(del);
   list.appendChild(li);
 
-  input.value = '';
+  input.value = "";
+  list_id++;
 });
